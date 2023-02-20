0811f8f09053d658e802a07aa0aad05bfa8e2cca
Michal Hudak <michal.hudak@bluelemons.sk>
2023-02-20 17:56:32 +0100

    first pages and layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="layout">
+    <div class="menu">
+      <nuxt-link to="/">
+        Home
+      </nuxt-link>
+      <nuxt-link to="/about">
+        About
+      </nuxt-link>
+    </div>
+    <nuxt />
+  </div>
+</template>
+    <nuxt />
+  </div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>About</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'AboutPage'
 }
 </script>
